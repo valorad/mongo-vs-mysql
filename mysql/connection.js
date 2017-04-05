d6d26355ba354167f64d51011502cb05fca658dc
Valorad <wcxaaa@gmail.com>
2017-04-05 23:31:58 +0800

    + connection to mysql

--- mysql/connection.js
+++ mysql/connection.js
@@ -1,1 +1,22 @@
 // connection to mysql
+const mysql = require('mysql');
+const fs = require('fs');
+const path = require('path');
+
+const file = path.join(__dirname,'../config/mongoVSmysql.json');
+const siteConfig = JSON.parse(fs.readFileSync(file, 'utf8'));
+
+const mvm = siteConfig[0];
+
+
+// create connection
+
+const mysqlInstance = mysql.createConnection({
+  host     : mvm.host,
+  user     : mvm.user,
+  password : mvm.password,
+  database : mvm.database
+});
+
+module.exports = mysqlInstance;
+// has to call connect() and end() in further tests.
--- test/connection.test.js
+++ test/connection.test.js
@@ -1,8 +1,17 @@
 //const mongoose = require("mongoose");
 var mongoInstance;
+var mysqlInstance;
 
-describe("make connection to databases", () => {
+describe("Mongo's turn", () => {
     it('connection to mongo', () => {
         mongoInstance = require("../mongo/connection");
+    });
+});
+
+describe("MySQL's turn", () => {
+    it('connection to mysql', () => {
+        mysqlInstance = require("../mysql/connection");
+        mysqlInstance.connect();
+        mysqlInstance.end();
     });
 });
